f0347ced2a7cd21395f9e0b736f4b85167c670ce
EzzEl-DeenMohamed <Ezzasdezz123@gmail.com>
2023-12-15 16:32:01 +0200

    make update on the SJFS to Print wait time and turnaround time

--- SJFScheduler.java
+++ SJFScheduler.java
@@ -10,8 +10,12 @@ public class SJFScheduler extends Scheduler {
     @Override
     public void runScheduler() {
         readyQueue = new LinkedList<>();
+        dieList = new ArrayList<>();
         int currentTime = 0;
+        int totalWaitingTime = 0;
+        int totalTurnaroundTime = 0;
 
+        System.out.println("Processes Execution Order:");
         while (!processes.isEmpty() || !readyQueue.isEmpty()) {
             // Add arrived processes to the readyQueue
             while (!processes.isEmpty() && processes.get(0).arrivalTime <= currentTime) {
@@ -26,13 +30,33 @@ public class SJFScheduler extends Scheduler {
                     }
                 }
 
-                System.out.println("Executing process " + shortest.name +" from time " + currentTime + " to " + (currentTime + shortest.burstTime));
+                System.out.println("Executing process " + shortest.name + " from time " + currentTime + " to " + (currentTime + shortest.burstTime));
                 currentTime += shortest.burstTime;
                 readyQueue.remove(shortest);
                 dieList.add(shortest);
+
+                int waitingTime = currentTime - shortest.arrivalTime - shortest.burstTime;
+                int turnaroundTime = currentTime - shortest.arrivalTime;
+
+                totalWaitingTime += waitingTime;
+                totalTurnaroundTime += turnaroundTime;
+
+                System.out.println("Waiting Time for " + shortest.name + ": " + waitingTime);
+                System.out.println("Turnaround Time for " + shortest.name + ": " + turnaroundTime);
             } else {
                 currentTime++; // No process available yet, move to the next time unit
             }
+        }
+
+        double avgWaitingTime = (double) totalWaitingTime / dieList.size();
+        double avgTurnaroundTime = (double) totalTurnaroundTime / dieList.size();
+
+        System.out.println("\nAverage Waiting Time: " + avgWaitingTime);
+        System.out.println("Average Turnaround Time: " + avgTurnaroundTime);
+
+        System.out.println("\nHistory update of quantum time for each process:");
+        for (Process p : dieList) {
+            System.out.println("Process " + p.name + ": Quantum Time Updated");
         }
     }
 }
